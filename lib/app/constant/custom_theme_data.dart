13e6d0fb5645def478d57bec91b965fd7d6cf759
Kim Yeseul <imys0613@naver.com>
2021-07-29 20:49:18 +0900

    [ADD] CUSTOM THEME

---
+++ lib/app/constant/custom_theme_data.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+final primaryColor = Colors.grey.shade50;
+final primaryColorLight = Colors.white;
+final primaryColorDark = Colors.grey.shade500;
+final primaryTextColor = Colors.black;
+final secondaryColor = Colors.blue.shade800;
+final secondaryColorLight = Colors.blue.shade700;
+final secondaryColorDark = Colors.blue.shade900;
+final secondaryTextColor = Colors.white;
+
+
+
+final customThemeData = ThemeData(
+    appBarTheme: AppBarTheme(
+      elevation: 1,
+      centerTitle: true,
+    ),
+    primaryColor: primaryColorLight,
+    scaffoldBackgroundColor: primaryColor,
+    accentColor: secondaryColor,
+    buttonTheme: ButtonThemeData(
+      buttonColor: secondaryColor,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: secondaryColor,
+      ),
+    ),
+    toggleButtonsTheme: ToggleButtonsThemeData(
+      selectedBorderColor: secondaryColor,
+      selectedColor: secondaryTextColor,
+      disabledColor: primaryColorLight,
+      fillColor: secondaryColor,
+    ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tutum_app/app/constant/custom_theme_data.dart';
 import 'package:tutum_app/app/routes/app_pages.dart';
 import 'package:tutum_app/services/auth_service.dart';
 import 'package:tutum_app/services/sensor/gps_service.dart';
+
 
 void main() {
   runApp(
     GetMaterialApp(
       title: 'Tutum 안전관리 앱',
-      defaultTransition: Transition.rightToLeft,
+      defaultTransition: Transition.rightToLeftWithFade,
       // route 설정
       initialRoute: Routes.TEST,
       getPages: AppPages.routes,
@@ -18,6 +20,7 @@ void main() {
           Get.put(GpsService());
         },
       ),
+      theme: customThemeData,
     ),
   );
 }
